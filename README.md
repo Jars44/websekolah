dc901cea20cd941e86cd04aa7d1bc89790ff4e85
mochraiyan <mra.alfikri@gmail.com>
2025-07-31 19:40:43 +0700

    Added LICENSE.txt & .editorConfig, fixed progress bar

---
+++ README.md
@@ -0,0 +1,35 @@
+# websekolah
+
+Web design competition prototype repository
+
+## Roadmap
+
+**Design**
+- [ ] Coloring
+- [ ] Fonts
+
+**Layout**
+- [X] Header
+- [X] Footer
+- [X] Hero
+- [X] Information Section
+- [X] Facility Section
+- [X] News Section
+- [ ] History Section
+- [ ] Video Section
+- [ ] Jurusan page
+  - [ ] Rekayasa Perangkat Lunak
+  - [ ] Desain Komunikasi Visual
+  - [ ] Elektronika Industri
+  - [ ] Broadcasting
+  - [ ] Teknik Komputer dan Jaringan
+  - [ ] Audio Video
+  - [ ] Mekatronika
+  - [ ] Animasi
+- [ ] News Page
+- [ ] MORE!
+
+## License
+
+Distributed under the MIT License. See `LICENSE.txt` for more information.
+
--- index.js
+++ index.js
@@ -4,18 +4,18 @@ document.addEventListener("DOMContentLoaded", function () {
   const nextBtn = document.querySelector(".carousel-btn.next");
 
   if (carousel && prevBtn && nextBtn) {
-      const card = carousel.querySelector(".jurusan-card");
-      const cardStyle = window.getComputedStyle(card);
-      const cardMargin = parseFloat(cardStyle.marginRight) || 25;
-      const scrollAmount = card.offsetWidth + cardMargin;
+    const card = carousel.querySelector(".jurusan-card");
+    const cardStyle = window.getComputedStyle(card);
+    const cardMargin = parseFloat(cardStyle.marginRight) || 25;
+    const scrollAmount = card.offsetWidth + cardMargin;
 
-      nextBtn.addEventListener("click", () => {
-          carousel.scrollBy({ left: scrollAmount, behavior: 'smooth' });
-      });
+    nextBtn.addEventListener("click", () => {
+      carousel.scrollBy({ left: scrollAmount, behavior: "smooth" });
+    });
 
-      prevBtn.addEventListener("click", () => {
-          carousel.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
-      });
+    prevBtn.addEventListener("click", () => {
+      carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
+    });
   }
 
   const menuBtn = document.querySelector(".btn-menu");
