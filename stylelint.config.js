f49dbed672e5d934b46fcd13c3e64667fccc54be
Jars44 <maulidfajar163@gmail.com>
2025-08-02 13:31:24 +0700

    `Removed .stylelintrc.json file and disabled stylelint configuration in web-check.yml workflow`

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,10 +1,10 @@
-{
-  "extends": "stylelint-config-standard",
-  "rules": {
+/** @type {import('stylelint').Config} */
+export default {
+  rules: {
     "indentation": 2,
     "color-no-invalid-hex": true,
     "string-quotes": "double",
     "block-no-empty": true,
     "no-extra-semicolons": true
   }
-}
+};
